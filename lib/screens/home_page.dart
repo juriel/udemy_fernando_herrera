8e9fc334969c6ff89c7c99dc4c4dda4adc2fb17c
juriel <juriel@comtor.net>
2022-08-26 13:47:13 -0500

    Se centra el contenido de la columna

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      appBar: AppBar(title: const Text("Curso Udemy")),
-      body: Center(child: Text("Hola Mundo 2"))
-      );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Curso Udemy"),
+          elevation: 5.0,
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Fila 1 "),
+            Text("Número de Clicks "),
+            Text("Hola Mundo "),
+          ],
+        )));
   }
-    
 }
